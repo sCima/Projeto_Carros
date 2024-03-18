299ba85d1ffde2138e24718facf253ebfdba7596
Lucas Lima <lucasdelimabzr@gmail.com>
2024-03-18 16:23:53 -0300

    Correção do parâmetro do método

--- src/br/com/carros/combustiveis/Alcool.java
+++ src/br/com/carros/combustiveis/Alcool.java
@@ -3,8 +3,8 @@ package br.com.carros.combustiveis;
 public class Alcool implements Combustivel {
 
 	@Override
-	public double consumo(double tanque) {
-		return tanque * 6;
+	public double consumo(double litros) {
+		return litros * 6;
 	}
 
 	
